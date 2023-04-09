6482eda0106bb15249c0bd4e3475b3943e167074
Faycal-Bezleghdir <b-faycal@live.fr>
2023-04-09 17:25:36 -0400

    creation de menu de notre page

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,54 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="container">
+    <nav class="navbar navbar-expand-lg  bg-body-tertiary bg-dark">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="/">
+          <img src="./assets/logo.png" alt="Bootstrap" width="100" height="30">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+      </div>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav text-white me-auto mb-2 mb-lg-0">
+          <li class="nav-item" >
+            <router-link class="nav-link text-white" to="/">Maison</router-link>
+          </li>
+          <li class="nav-item text-nowrap">
+            <router-link class="nav-link text-white" to="/articles">Des articles</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link text-white" to="/commentaire">Commentaire</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link text-white" to="/employee">Employé</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link text-white" to="/inventaire">Inventaire</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link text-white" to="/facture">Facture</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link text-white" to="/role">Rôle</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link text-white" to="/salaire">Salaire</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link text-white" to="/utilisateurs">Utilisateurs</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+  <router-view />
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+<style scoped>
+.active {
+  color: #f48c24 !important
 }
 </style>
