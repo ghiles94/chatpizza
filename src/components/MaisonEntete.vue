c1b0169ce8058f1d8909e6f9a45e5663aa454347
ghiles94 <ghiles666666@gmail.com>
2023-04-09 16:59:06 -0700

    creation de la page d'acceuil

---
+++ src/components/MaisonEntete.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="mt-5">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MaisonEntete',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  color: #f48c24;
+}
+</style>
---
+++ src/views/MaisonView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <MaisonEntete msg="Bienvenue à Pizza Chat"/>
+  </div>
+</template>
+
+<script>
+import MaisonEntete from '../components/MaisonEntete.vue'
+
+export default {
+  name: 'MaisonView',
+  components: {
+    MaisonEntete
+  }
+}
+</script>
