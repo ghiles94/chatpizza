f360e8f69839950e678b319ce2ac2ef432c7a47b
Faycal-Bezleghdir <b-faycal@live.fr>
2023-04-11 18:06:53 -0400

    utilisation bootstrap

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
+
 import App from './App.vue'
+
 import router from './router'
 import 'bootstrap-icons/font/bootstrap-icons.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap'
 
-createApp(App).use(router).mount('#app')
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import 'bootstrap'
